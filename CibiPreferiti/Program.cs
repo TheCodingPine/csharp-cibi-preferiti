0d8461b6122dfb2e7ce201f7c391dd44d3f1e896
Alberto Rosa <alberto.r.93@hotmail.it>
2022-04-01 17:08:52 +0200

    bonus

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-string[] cibiPreferiti = {"Cioccolata", "Pizza", "Bagnacauda", "Pasta", "Pesce"};
+string[] cibiPreferiti = {"Cioccolata", "Pizza", "Bagnacauda", "Pasta", "Pollo", "Pesce"};
 
 Console.WriteLine("Esercizio 1");
 
@@ -24,4 +24,17 @@ Console.WriteLine("In prima posizione abbiamo " + firstClass);
 var lastClass = cibiPreferiti[lunghezza-1];
 Console.WriteLine("In ultima posizione abbiamo " + lastClass);
 
-//
+//stampare il median
+
+int midNumber = lunghezza / 2; //il numero a metà
+int resto = lunghezza % 2; // modulo
+int medianoAssoluto = lunghezza - midNumber;
+int theFirsteMediano = midNumber - 1;
+
+if (resto == 1)
+{
+    Console.WriteLine("Il mediano dispari è " + cibiPreferiti[midNumber]);
+
+} else {
+    Console.WriteLine("Il mediano pari è " + cibiPreferiti[theFirsteMediano] + " e " + cibiPreferiti[midNumber]);
+}
