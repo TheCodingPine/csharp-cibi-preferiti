b00cd308a8bf3371bc3d3fcb4609506d80e55433
Alberto Rosa <alberto.r.93@hotmail.it>
2022-04-01 16:10:18 +0200

    lista e length

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string[] cibiPreferiti = { "Pizza", "Bagnacauda", "Pasta", "Cioccolata", "Pesce" };
+
+Console.WriteLine("Esercizio 1");
+
+var lunghezza = cibiPreferiti.Length + 1;
+
+Console.WriteLine("La stringa è lunga " + lunghezza);
