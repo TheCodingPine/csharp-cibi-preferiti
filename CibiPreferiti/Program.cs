9aa6f27f35e5ebf5f8a7a9c974c579caec77eed8
Alberto Rosa <alberto.r.93@hotmail.it>
2022-04-01 16:37:34 +0200

    complete

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -1,10 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-string[] cibiPreferiti = { "Pizza", "Bagnacauda", "Pasta", "Cioccolata", "Pesce" };
+string[] cibiPreferiti = {"Cioccolata", "Pizza", "Bagnacauda", "Pasta", "Pesce"};
 
 Console.WriteLine("Esercizio 1");
 
-var lunghezza = cibiPreferiti.Length + 1;
+var lunghezza = cibiPreferiti.Length;
 
+// lunghezza della stringa
 Console.WriteLine("La stringa è lunga " + lunghezza);
+
+//elenco della lista
+for (int i = 0; i < lunghezza; i++)
+{
+    Console.WriteLine(cibiPreferiti[i]);
+}
+
+//first class
+var firstClass = cibiPreferiti[0];
+Console.WriteLine("In prima posizione abbiamo " + firstClass);
+
+//last class
+var lastClass = cibiPreferiti[lunghezza-1];
+Console.WriteLine("In ultima posizione abbiamo " + lastClass);
+
+//
